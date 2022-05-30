5464a326df85274454a82779e594a2289e121f48
CudraMohamed <ncudramohamed@gmail.com>
2022-05-30 21:19:52 +0300

    first commit

---
+++ hello.js
@@ -0,0 +1,15 @@
+// var message="Hello"
+// console.log(message)
+// alert(message)
+// var name = "Anyone";
+// document.getElementById('demo').innerHTML="Hello there";
+
+// function our_class(){
+//     var name ="AnitaB";
+//     console.log()
+// }
+// our_class(name)
+// document.getElementById("button").addEventListener('click',function(){
+//     document.write("Hey there");
+    
+// })
