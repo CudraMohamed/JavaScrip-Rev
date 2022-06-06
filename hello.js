af6d346606ba178b5b68916228d0664cb03c5284
CudraMohamed <ncudramohamed@gmail.com>
2022-06-06 22:47:09 +0300

    operators

--- hello.js
+++ hello.js
@@ -13,3 +13,51 @@
 //     document.write("Hey there");
     
 // })
+var message = 'Hello';
+var name = 'Cudra';
+var num = 4;
+var sen = `My name is $(name)`;
+console.log(message+name);
+console.log(name+num);
+console.log(typeof(name+num));
+console.log('How are you ',name)
+console.log(sen)
+
+//Name is a reversed variable hence if you use it it brings an underline
+
+//conditional/ternary opersators
+let kids = 23
+ let child = kids? 23: falseconsole.log(child);
+let diff = (kids > 50)? 'these are many kids':'not enough kids';
+console.log(diff)
+
+var  x = [0,1,2,3,4,5,6,7,8,9]
+for(let i = 0 ; i<x.length;i++){
+    console.log(x)
+}
+
+let student ={
+    name:"Cudra",
+    age:20,
+    class:'AnitaB'
+};
+
+console.log('age' in student)
+
+delete student.name  //deleting an object
+console.log(student);
+
+console.log(typeof(student));
+
+var today = new Date().toLocaleTimeString;
+console.log(today)
+
+// void expression
+
+var  trees = [  'redwood'  ,  'bay'  ,  'cedar'  ,  'oak'  ,  'maple'  ]; 
+console.log([11] in trees)
+
+//in objects we use the items to access the item
+
+// var today = the ,Date();
+// let t = (today instanceof Date?)
