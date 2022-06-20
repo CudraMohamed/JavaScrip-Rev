410d76db7bc5b25e6c70cb6ef1ce3119f1a24942
CudraMohamed <ncudramohamed@gmail.com>
2022-06-20 20:20:25 +0300

    added loops

--- hello.js
+++ hello.js
@@ -61,3 +61,154 @@ console.log([11] in trees)
 
 // var today = the ,Date();
 // let t = (today instanceof Date?)
+
+
+//Global and local variable
+//global var is accessible anywhere
+var name = "Conslate";
+function me(){
+    //document.write("hey there")
+    var car = "Toyota";
+    console.log(name)
+}
+me()
+// console.log(car)
+
+//declaring JavaScript variable
+function practice(){
+    var x = 50; //available before declaration
+    x=20 
+    console.log(x)
+    let p = 40;
+    const m =30;
+
+}
+
+//types and value
+var a = null
+//a doesn't have a type but null has a type which is object
+
+var b;
+b = 4
+console.log(b)
+
+//Undefined error- if a value is not declared or gassigned a value
+
+//Arrays-a collection of data types
+let numb = [1,2,"3",[9]]; //2D array
+console.log(numb.length);
+console.log(numb[3][0]);
+value_2d=numb[3][0] //accessing 2D Arrays
+console.log(value_2d)
+ 
+let namsey = [`you`,[`Them`,[`guys`]]] //3D array
+console.log(namsey[1][0][0])
+
+let p ='foo'
+let t = p.concat(`bar`) //concatination
+console.log(t)
+
+//classes
+function Pupil(){
+    constructor(name);
+        this.name=name;
+}
+
+pupil=new Pupil("Cudra");
+console.log(pupil.name);
+Pupil.prototype.gender=`female`;    //prototypes
+console.log(pupil.gender);
+
+//converting between type
+var q = `45`;
+console.log(Number(a)+3);   //explicit coercion - changing into a number but it remains to be as defined
+console.log(typeof(a));
+
+let v = 4;
+let w = 4 + `hey`;  //implicit coercion
+console.log(w);
+console.log(Number(w))
+console.log(typeof(w))
+
+//you can change a number to a string
+//this is a single line  comments
+/*multiple
+line comments*/
+
+//take a list
+//create a counter with a variable sum = 0
+//iterate variable string and add elements to sum variable
+//console sum
+
+
+//Conditional statements
+let my_name = "Cudra"
+if (my_name != `Cynthia`){
+    console.log(`Hey there`);
+    if(my_name.lenght <=3){
+        console.log("Name is too short");
+    }else{
+        console.log("Name not accepted")
+    }
+}
+
+let g = 20;
+let f = 10;
+let add = g+f
+//&& -> and ->both conditions are true
+//|| -> or -> either one or both are true
+
+if (add <= 30 || a >20){
+    console.log(true);
+}
+else{
+    console.log(false);
+}
+
+//SWITCH STATEMENTS
+// let l = 20;
+var grade = 'B';
+    console.log("Entering switch block");
+switch (grade) {        //used when you have one condition- It is a single expression
+    case 'A': console.log("Good job");
+    break;
+ 
+    case 'B': console.log("Pretty good");
+    break;
+ 
+    case 'C': console.log("Passed");
+    break;
+ 
+    case 'D': console.log("Not so good");
+    break;
+ 
+    case 'F': console.log("Failed");
+    break;
+ 
+    default:  console.log("Unknown grade")
+ }
+
+ see=10;
+ you=20;
+
+ switch(true){
+    case see==10:
+    console.log("Yes it is")
+    break;
+    case you <=20:
+        console.log("you")
+        break;
+
+    default:
+        console.log("Bye")
+        break;
+ }
+
+ //TERNARY OPERATOR
+ const amount=20;
+ let amount_b = amount>20 ? true : false;
+ console.log(amount_b)
+
+ var age = 26;
+ let beverage = age >=21 ? "Beer":"Juice";
+ console.log(beverage)
---
+++ loops.js
@@ -0,0 +1,83 @@
+//triple equal signs check for value and equality of the values ===
+
+function random(){
+const materials = ["Lockers","tables","Chairs","Computerss","Television"];
+const food = ["Chapati","Ugali","Chicken","Fish","CHips"];
+const persons ={
+
+him :"John",
+age:20,
+
+}
+
+
+outerloop:
+for (j=0;j<food.length;j++){
+    for (i=0;materials.length;i++){
+        
+        if(i===1){
+            break outerloop;            //label  statement
+        }
+        console.log(food[j],materials[i])
+    }
+}
+
+        //For loop
+        // for (let p = 0; p < materials.length; p++){
+            // console.log(materials[i]);       while using break -> the statement comes before the break
+            // if (i===2){
+            //     break;
+            // }
+        //     if (p===3){
+        //         continue;
+        //     }
+        //     console.log(materials[p])
+        // }
+
+        //do...while loop
+        let a=0                             //defining variable
+        do{
+            console.log(materials[a])       //statement
+            a++
+        }
+        while(a<materials.length){            //condition
+        }
+
+
+        //for ....in statement
+      for (x in materials){     //prints out the values #arrays do
+        console.log(x)
+      }  
+    
+        //for ....of statement
+        for (y of materials){       //prints the elements
+            console.log(y)
+        }
+        //printing objects
+        for(prop in persons){
+            console.log(persons[prop])
+        }
+
+}
+//recursion ->  revocking a function
+//function scope -> variable in a function can't be used outside the function
+//Closure -> Nesting functions
+//arguments object -> all the variables are added here and can be accessed 
+function square(number){
+    return number * number;
+}
+function rectangle () {
+    return arguments[0]*arguments[0];
+}
+const s = rectangle(1,2)
+console.log(s)
+
+function outerFunction(){
+    const s = "I am outer";
+
+    setTimeout(function(){
+        console.log(s)
+    },2000)
+}
+//function parameters
+//comparing values
